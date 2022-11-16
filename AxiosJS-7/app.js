9702e440e9739ba1d2c4dd0b4af701e29777b726
Pablo <pfl0002@alu.medac.es>
2022-11-16 20:19:53 +0100

    axios

--- AxiosJS-7/app.js
+++ AxiosJS-7/app.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
-const pokemon = async () => {
-    await axios.get('https://pokeapi.co/api/v2/pokemon?limit=999').then((responde) => {
-        console.log(responde.data);
-    })
+const pokemons = []
+async function pokemon() {
+    await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100').then(response => pokemons.push(response.data.results)
+    )
 }
-console.log(pokemon());
+pokemon().then(() => console.log(pokemons[0][0].name + " " + pokemons[0][0].url));
+
