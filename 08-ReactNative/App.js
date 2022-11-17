35956b2337835c70d8a97e7d3073a906f38b7338
Pablo <pablofernandezlopez766@gmail.com>
2022-11-17 13:22:45 +0100

    ReactNative

--- 08-ReactNative/App.js
+++ 08-ReactNative/App.js
@@ -1,48 +1,44 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ListItem, Avatar } from '@rneui/themed';
 const axios = require('axios').default;
 
-async function pokemonsSize() {
-  lista = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1').then(function (response) {
-    return response.data;
-  }).catch(function (e) {
-    console.log(e);
-  });
-  return lista;
-}
-
-pokemonsSize().then(async function (response) {
-  var list = [];
-  await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${response.count}`).then(function (response) {
-    list.push({ name: response.data.results[0].name, avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', subtitle: 'Vice President' });
-    list.push({ name: response.data.results[1].name, avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', subtitle: 'Vice President' });
-  }).catch(function (e) {
-    console.log(e);
-  });
-  return list.map((item, i) => {
-    <View>
-      {
-        <ListItem>
-          key={i}
-          title={item.name}
-          subtitle={item.subtitle}
-          leftAvatar={{ source: { uri: item.avatar_url } }}
-        </ListItem>
-      }
-    </View>
-  })
-});
-
-
-
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    for (let j = 0; j <= 4; j++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${j}`
+      axios.get(url)
+        .then(response => {
+          let jsonGenerator = [{ avatar_url: response.data.sprites.front_default, name: response.data.forms[0].name, subtitle: response.data.types[0].type.name }]
+          setPokemons(jsonGenerator)
+        })
+    }
+  }, []);
+
   return (
     <SafeAreaView>
       <ScrollView>
-        
+        <View>
+          {
+            pokemons.map((l, i) => (
+
+              <TouchableOpacity>
+                <ListItem key={i} style={styles.container}>
+                  <Avatar source={{ uri: l.avatar_url }} />
+                  <ListItem.Content>
+                    <ListItem.Title>{l.name}</ListItem.Title>
+                    <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+                  </ListItem.Content>
+                </ListItem>
+              </TouchableOpacity>
+            ))
+          }
+        </View>
       </ScrollView>
     </SafeAreaView>
+
   );
 }
 
