6bac24aeaf4c311871bb1fa13e749b7480948015
Pablo <pfl0002@alu.medac.es>
2022-11-17 19:48:42 +0100

    ReactNative

--- 08-ReactNative/App.js
+++ 08-ReactNative/App.js
@@ -1,35 +1,39 @@
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ListItem, Avatar } from '@rneui/themed';
 const axios = require('axios').default;
 
-export default function App() {
+export default function App({ navigation, params }) {
   const [pokemons, setPokemons] = useState([]);
-  useEffect(() => {
-    for (let j = 0; j <= 4; j++) {
-      const url = `https://pokeapi.co/api/v2/pokemon/${j}`
-      axios.get(url)
-        .then(response => {
-          let jsonGenerator = [{ avatar_url: response.data.sprites.front_default, name: response.data.forms[0].name, subtitle: response.data.types[0].type.name }]
-          setPokemons(jsonGenerator)
+  var pokemons_array = [];
+  useEffect(async () => {
+    for (let j = 1; j <= 100; j++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${j}/`
+      console.log(url);
+      await axios.get(url)
+        .then(async response => {
+          let jsonGenerator = { avatar_url: response.data.sprites.front_default, name: response.data.forms[0].name, subtitle: response.data.types[0].type.name };
+
+          pokemons_array.push(jsonGenerator);
+
         })
     }
+    setPokemons(pokemons_array);
   }, []);
 
   return (
     <SafeAreaView>
       <ScrollView>
         <View>
-          {
+          {pokemons.length == 0 ? <ActivityIndicator size="large" /> :
             pokemons.map((l, i) => (
-
               <TouchableOpacity>
                 <ListItem key={i} style={styles.container}>
                   <Avatar source={{ uri: l.avatar_url }} />
                   <ListItem.Content>
                     <ListItem.Title>{l.name}</ListItem.Title>
-                    <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+                    <ListItem.Subtitle> Tipo {l.subtitle}</ListItem.Subtitle>
                   </ListItem.Content>
                 </ListItem>
               </TouchableOpacity>
