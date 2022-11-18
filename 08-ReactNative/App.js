df5ffdefa3923f9783e20880651a989caaafecd3
Pablo <pfl0002@alu.medac.es>
2022-11-18 20:46:24 +0100

    React Native

--- 08-ReactNative/App.js
+++ 08-ReactNative/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Text, StyleSheet, View, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { ListItem, Avatar } from '@rneui/themed';
+import { ListItem, Avatar, Image } from '@rneui/themed';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function PokemonsScreen({ navigation }) {
@@ -52,13 +52,49 @@ function PokemonsScreen({ navigation }) {
 }
 
 function DetallesScreen({ route, navigation }) {
+  /* Abilitys */
   let idpokemon = route.params.id + 1;
+  const [pokemons, setPokemonsInfo] = useState([]);
+  var pokemons_array = [];
+  async function pokemonsInfo() {
+    const url = `https://pokeapi.co/api/v2/pokemon/${idpokemon}/`
+    const response = await fetch(url)
+    const data = await response.json()
+    let jsonGenerator = { avatar_url: data.sprites.front_default, name: data.forms[0].name.toUpperCase(), subtitle: data.types[0].type.name };
+    await pokemons_array.push(jsonGenerator);
+    setPokemonsInfo(pokemons_array);
+  }
+  useEffect(() => {
+    pokemonsInfo();
+    return () => {
+      true
+    };
+  }, []);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{idpokemon}</Text>
-      <Text></Text>
-    </View>
+    <ScrollView style={styles.container} >
+      <View>
+        {pokemons.length == 0 ? <ActivityIndicator size="large" /> :
+          pokemons.map((l, index) => (
+            <TouchableOpacity key={index} onPress={() => {
+              navigation.navigate('Detalles', { id: index });
+            }
+            }>
+              <ListItem containerStyle={{ backgroundColor: "rgba(189, 189, 189, 0.596);" }}>
+                <Avatar size="large"
+                  title="LW"
+                  activeOpacity={0.7} source={{ uri: l.avatar_url }} />
+                <ListItem.Content >
+                  <ListItem.Title style={styles.title}>{l.name}</ListItem.Title>
+                  <ListItem.Subtitle style={styles.subtitle}> Type: {l.subtitle}</ListItem.Subtitle>
+                </ListItem.Content>
+
+              </ListItem>
+            </TouchableOpacity>
+          ))
+        }
+      </View>
+    </ScrollView>
   );
 }
 
@@ -98,20 +134,20 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    color:'#fff'
+    color: '#fff'
   },
   subtitle: {
     fontSize: 15,
-    color:'rgb(204, 70, 70)',
+    color: 'rgb(204, 70, 70)',
     fontWeight: 'bold',
   },
   number: {
     fontSize: 13,
     textAlign: 'right',
-    color:'#fff',
+    color: '#fff',
   },
   text: {
-    color:'#fff',
+    color: '#fff',
   }
 
 
