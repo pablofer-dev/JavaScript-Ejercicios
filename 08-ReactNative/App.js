cf1a0a76e5c8ad24c2ca696f42eb287d536259c7
Pablo <pfl0002@alu.medac.es>
2022-11-18 18:46:55 +0100

    ReactNative

--- 08-ReactNative/App.js
+++ 08-ReactNative/App.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { useEffect, useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button, Text, StyleSheet, View, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { ListItem, Avatar } from '@rneui/themed';
-const fetch = require('node-fetch');
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App({ navigation, params }) {
+function HomeScreen({ navigation }) {
   const [pokemons, setPokemons] = useState([]);
   var pokemons_array = [];
   async function pokemonsPritn() {
@@ -30,29 +31,50 @@ export default function App({ navigation, params }) {
         <View>
           {pokemons.length == 0 ? <ActivityIndicator size="large" /> :
             pokemons.map((l, index) => (
-              <TouchableOpacity key={index}>
-                <ListItem style={styles.container}>
-                  <Avatar size="large"
-                    title="LW"
-                    onPress={() => console.log("Works!")}
-                    activeOpacity={0.7} source={{ uri: l.avatar_url }} />
-                  <ListItem.Content>
-                    <ListItem.Title styles={styles.title}>{l.name}</ListItem.Title>
-                    <ListItem.Subtitle> Type: {l.subtitle}</ListItem.Subtitle>
-                  </ListItem.Content>
-                </ListItem>
-              </TouchableOpacity>
-            ))
+              <TouchableOpacity key={index} onPress={() => {
+                navigation.navigate('Details')
+              }
+              }>
+          <ListItem style={styles.container}>
+            <Avatar size="large"
+              title="LW"
+              onPress={() => console.log("Works!")}
+              activeOpacity={0.7} source={{ uri: l.avatar_url }} />
+            <ListItem.Content>
+              <ListItem.Title styles={styles.title}>{l.name}</ListItem.Title>
+              <ListItem.Subtitle> Type: {l.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </TouchableOpacity>
+        ))
           }
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </ScrollView>
+    </SafeAreaView >
 
   );
 }
-<Avatar
 
-/>
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#000'
@@ -62,3 +84,4 @@ const styles = StyleSheet.create({
   },
 
 });
+export default App;
